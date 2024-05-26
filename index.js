cc98b47941be32a2ded7223d2399ed4d7ae31b62
Ankush Goel <goelankush1312@gmail.com>
2024-05-27 00:00:15 +0530

    Module import and export via commonjs

--- index.js
+++ index.js
@@ -3,5 +3,12 @@
 // console.log(document);
 
 //
-console.log(global);
-console.log(process);
+// console.log(global);
+// console.log(process);
+
+// Module import and export
+const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Celcius to fahrenhiet: ${celciusToFahrenheit(37)}`);
